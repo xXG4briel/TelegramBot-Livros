dcb4f2fe90fb804b78da38cb96a2d6521c74e608
xXG4brielgit config --global user.name xXG4brielgit config --global user.name xXG4briel <gabrielgeorge95@gmail.com>
2022-09-03 13:47:07 -0300

    teste 1 min

--- bot.py
+++ bot.py
@@ -11,15 +11,11 @@ def sendMessage():
     updater.bot.send_message(MY_GROUP_ID, "Olá")
 
 # Define os horários do dia que serão mandadas a mensagem
-schedule.every().day.at("06:00").do(sendMessage)
-schedule.every().day.at("12:00").do(sendMessage)
-schedule.every().day.at("12:00").do(sendMessage)
-schedule.every().day.at("12:41").do(sendMessage)
-schedule.every().day.at("13:00").do(sendMessage)
-schedule.every().day.at("13:10").do(sendMessage)
-schedule.every().day.at("13:20").do(sendMessage)
-schedule.every().day.at("13:30").do(sendMessage)
-schedule.every().day.at("18:00").do(sendMessage)
+# schedule.every().day.at("06:00").do(sendMessage)
+# schedule.every().day.at("12:00").do(sendMessage)
+# schedule.every().day.at("18:00").do(sendMessage)
+# schedule.every().to(6).hours.do(sendMessage)
+schedule.every(1).minutes.do(sendMessage)
 
 def main():
     updater.start_polling()
