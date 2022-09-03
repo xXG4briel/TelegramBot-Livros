a52c213827df0beb771a74800205d3d1ae0a4654
xXG4brielgit config --global user.name xXG4brielgit config --global user.name xXG4briel <gabrielgeorge95@gmail.com>
2022-09-03 13:52:25 -0300

    asdas

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ from telegram.ext import Updater, CommandHandler, InlineQueryHandler, MessageHan
 from time import sleep
 import schedule
 
-MY_TOKEN_ID = open("./BOT_INFO.txt", "r").readlines()[0].replace("\n","")
-MY_GROUP_ID = open("./BOT_INFO.txt", "r").readlines()[1]
+MY_TOKEN_ID = "5770561614:AAFuGC5eut7RwOf7UNi7_eglCVAnGjTESis"
+MY_GROUP_ID = '-682460728'
 updater = Updater(MY_TOKEN_ID)
 
 
@@ -15,7 +15,7 @@ def sendMessage():
 # schedule.every().day.at("12:00").do(sendMessage)
 # schedule.every().day.at("18:00").do(sendMessage)
 # schedule.every().to(6).hours.do(sendMessage)
-schedule.every(1).minutes.do(sendMessage)
+schedule.every(30).seconds.do(sendMessage)
 
 def main():
     updater.start_polling()
